0bbcae41d68691f084d95b978d0b13bf50bc1ed3
laevad <77134175+laevad@users.noreply.github.com>
2023-06-23 09:29:07 +0800

    create container class navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,18 @@
 export const Navbar = () => {
 	return (
 		<>
-			<div className="navbar bg-base-100">
-				<div className="flex-1">
-					<a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-				</div>
-				<div className="flex-none gap-2">
-					<div className="form-control">
-						<input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+			<header>
+				<div className="navbar bg-base-100 container">
+					<div className="flex-1">
+						<a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+					</div>
+					<div className="flex-none gap-2">
+						<div className="form-control">
+							<input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+						</div>
 					</div>
 				</div>
-			</div>
+			</header>
 		</>
 	)
 }
