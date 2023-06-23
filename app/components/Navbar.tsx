378fe81dab59606020c7c6597dd72e5e46d70da6
laevad <77134175+laevad@users.noreply.github.com>
2023-06-23 11:13:33 +0800

    partial - use client

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,9 @@
+"use client";
 import Link from "next/link";
+import {useState} from "react";
 
 export const Navbar = () => {
+	const [querySearch, setQuerySearch] = useState<string>("");
 	return (
 		<>
 			<header>
@@ -12,7 +15,9 @@ export const Navbar = () => {
 					</div>
 					<div className="flex-none gap-2">
 						<div className="form-control">
-							<input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+							<input
+								value={""}
+								type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
 						</div>
 					</div>
 				</div>
