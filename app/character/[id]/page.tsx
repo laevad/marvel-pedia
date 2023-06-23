96312c108ce47abb34fd83e7b5b5d49a191bd498
laevad <77134175+laevad@users.noreply.github.com>
2023-06-23 11:05:56 +0800

    design the character page

--- app/character/[id]/page.tsx
+++ app/character/[id]/page.tsx
@@ -9,10 +9,22 @@ interface CharacterPageProps {
 export default async function page({params}: CharacterPageProps) {
     const {id} = params;
     const character = await getCharacterById(id);
-    console.log(character);
+    const {thumbnail, name, description} = character.results[0];
     return (
         <>
-            <div className="">Page!</div>
+            <div className="container flex flx-col gap-5 items-center">
+                <div className="card shadow-xl w-full">
+                    <figure>
+                        <img src={`${thumbnail.path}.${thumbnail.extension}`} alt={name}
+                             className="object-contain w-10/12 h-96" loading={"lazy"}
+                        />
+                    </figure>
+                    <div className="card-body mx-auto max-w-4xl text-center">
+                        <h2 className="text-3xl font-bold mb-4">{name}</h2>
+                        <p className="text-sm font-light">{description}</p>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
