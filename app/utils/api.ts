a9e728196019f6d55cc7b8b8e1dc1894766fd5d7
laevad <77134175+laevad@users.noreply.github.com>
2023-06-23 09:50:29 +0800

    setup initially the api (ERROR)

---
+++ app/utils/api.ts
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://gateway.marvel.com/v1/public/';
+API_PUBLIC_KEY="c2a0e16138109fe8c958a57e370ebaa1";
+API_PRIVATE_KEY="76f51f5b5c1945682418118a783474d023692c2b";
+
+
+export const getCharacters = async () => {
+
+}
