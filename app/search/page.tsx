9c4eff29e4d92e132dadfefac7c3164882dea0fd
laevad <77134175+laevad@users.noreply.github.com>
2023-06-23 11:29:48 +0800

    partial - setup use effect search

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,7 +1,34 @@
+"use client";
+import {useRouter, useSearchParams} from "next/navigation";
+import {useEffect} from "react";
+
 interface SearchPageProps {
 
 }
 
 export default function SearchPage({}: SearchPageProps) {
+    const searchParams = useSearchParams();
+    const query = searchParams.get("query");
+    const router = useRouter();
 
+    useEffect(() => {
+        const fetchSearch = async () => {
+            try {
+
+            }
+            catch (e) {
+
+            }
+        }
+
+    }, [query]);
+
+
+    return (
+        <>
+            <div className="container text-center mt-10">
+                <h1 className={"text-3xl font-bold"}>Search for <span>&quot;{ query }&quot;</span></h1>
+            </div>
+        </>
+    );
 }
