d13567bbffa492f1fc6c2d3fb392193d5d51166c
laevad <77134175+laevad@users.noreply.github.com>
2023-06-23 09:44:17 +0800

    removed the image for a while

--- app/components/CharacterCard.tsx
+++ app/components/CharacterCard.tsx
@@ -3,7 +3,6 @@ export const CharacterCard = () => {
 		<>
 			<div className="card card-compact w-full bg-base-100 shadow-xl">
 				<figure>
-					{/*<img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />*/}
 				</figure>
 				<div className="card-body">
 					<h2 className="card-title">Shoes!</h2>
