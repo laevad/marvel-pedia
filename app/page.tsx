aa910079160b09073e45b46d8f76dd90d70680c8
laevad <77134175+laevad@users.noreply.github.com>
2023-06-23 09:33:26 +0800

    design the header

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <div className={"container text-center"}>
+          <h1 className={"text-3xl font-bold underline"}>Popular Character Marvels</h1>
+      </div>
     </main>
   )
 }
