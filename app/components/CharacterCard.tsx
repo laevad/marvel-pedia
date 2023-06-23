fddf3a19afc5515ae1cc5a54984890d9d784707d
laevad <77134175+laevad@users.noreply.github.com>
2023-06-23 09:43:04 +0800

    setup card design

--- app/components/CharacterCard.tsx
+++ app/components/CharacterCard.tsx
@@ -1,7 +1,18 @@
 export const CharacterCard = () => {
 	return (
 		<>
-			<div>Character Card</div>
+			<div className="card card-compact w-full bg-base-100 shadow-xl">
+				<figure>
+					{/*<img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />*/}
+				</figure>
+				<div className="card-body">
+					<h2 className="card-title">Shoes!</h2>
+					<p>If a dog chews shoes whose shoes does he choose?</p>
+					<div className="card-actions justify-end">
+						<button className="btn btn-primary">Buy Now</button>
+					</div>
+				</div>
+			</div>
 		</>
 	)
 }
