69eb81149b998804b72e1f2ab42a18817a4f0219
laevad <77134175+laevad@users.noreply.github.com>
2023-06-23 13:26:57 +0800

    error loading , not found

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+
+import { FC } from 'react';
+import {LoadingBars} from "@/app/components/LoadingBars";
+
+const loading: FC = () => {
+    return <LoadingBars  className='h-screen' />;
+};
+
+export default loading;
