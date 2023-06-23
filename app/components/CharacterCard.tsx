1c4b6a58e33e1b4b93273114e083596009fdb450
laevad <77134175+laevad@users.noreply.github.com>
2023-06-23 10:19:20 +0800

    setup interface

--- app/components/CharacterCard.tsx
+++ app/components/CharacterCard.tsx
@@ -1,14 +1,19 @@
-export const CharacterCard = () => {
+import {Character} from "@/app/type/marvels";
+
+interface CharacterCardProps {
+	character: Character;
+}
+
+export const CharacterCard = ({character}: CharacterCardProps) => {
 	return (
 		<>
 			<div className="card card-compact w-full bg-base-100 shadow-xl">
 				<figure>
 				</figure>
 				<div className="card-body">
-					<h2 className="card-title">Shoes!</h2>
-					<p>If a dog chews shoes whose shoes does he choose?</p>
+					<h2 className="card-title">{character.name}</h2>
 					<div className="card-actions justify-end">
-						<button className="btn btn-primary">Buy Now</button>
+						<button className="btn btn-primary">Detail { character.name }</button>
 					</div>
 				</div>
 			</div>
