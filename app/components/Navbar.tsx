b0c63fdef9f488bf673b05b6eb56c74832ddc566
laevad <77134175+laevad@users.noreply.github.com>
2023-06-23 11:16:42 +0800

    partial - on keydown

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,15 @@
 "use client";
 import Link from "next/link";
-import {useState} from "react";
+import React, {useState} from "react";
 
 export const Navbar = () => {
 	const [querySearch, setQuerySearch] = useState<string>("");
+
+	const handleSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === "Enter" && querySearch !== "") {
+
+		}
+	}
 	return (
 		<>
 			<header>
@@ -16,8 +22,11 @@ export const Navbar = () => {
 					<div className="flex-none gap-2">
 						<div className="form-control">
 							<input
-								value={""}
-								type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+								value={querySearch}
+								type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto"
+								onChange={(e) => setQuerySearch(e.target.value)}
+								onKeyDown={handleSearch}
+							/>
 						</div>
 					</div>
 				</div>
